c7bc9f212429293c662b4004cb4546aaa95fbaeb
joao.gomes <joao.melquiades@ee.ufcg.edu.br>
2023-02-08 23:21:35 -0300

    chore: Organize files into different directories

--- sv/sequences/axi4_lite_wr_addr_master_sequence.sv
+++ sv/sequences/axi4_lite_wr_addr_master_sequence.sv
@@ -1,29 +1,29 @@
 /********************************************** /
 AXI4-Lite VIP
 
-file: axi4_lite_wr_addr_channel_sequence.sv
+file: axi4_lite_wr_addr_master_sequence.sv
 author: João Pedro Melquiades Gomes
 mail: jmelquiadesgomes@gmail.com
 
 Description: Sequencer that will manage the sequences
 of the AXI4 Lite
 ************************************************/
-class axi4_lite_wr_addr_channel_sequence extends axi4_lite_base_sequence;
-    `uvm_object_utils(axi4_lite_wr_addr_channel_sequence);
+class axi4_lite_wr_addr_master_sequence extends axi4_lite_base_sequence;
+    `uvm_object_utils(axi4_lite_wr_addr_master_sequence);
 
     //  Constructor: new
-    function new(string name = "axi4_lite_wr_addr_channel_sequence");
+    function new(string name = "axi4_lite_wr_addr_master_sequence");
         super.new(name);
     endfunction: new
     /*
     Task: body
     Description: This task will randomize the req transaction with
-    the channel set to WR_ADDR
+    the master set to WR_ADDR
     */
     extern task body();
 
-endclass: axi4_lite_wr_addr_channel_sequence
+endclass: axi4_lite_wr_addr_master_sequence
 
-task axi4_lite_wr_addr_channel_sequence::body();
-    `uvm_do_with(req, req.active_channel == WR_ADDR)
+task axi4_lite_wr_addr_master_sequence::body();
+    `uvm_do_with(req, req.active_master == WR_ADDR)
 endtask : body

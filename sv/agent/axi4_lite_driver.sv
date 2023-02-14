c11444a876042252483d8d549006019db0eae54f
joao.gomes <joao.melquiades@ee.ufcg.edu.br>
2023-02-14 20:42:17 -0300

    feat: Implemented driver

--- sv/agent/axi4_lite_driver.sv
+++ sv/agent/axi4_lite_driver.sv
@@ -9,9 +9,12 @@ Description: The driver is capable of drive to master and
 slave AXI4 Lite interfaces. It will be pipelined to drive
 multichannels in parallel.
 ************************************************/
+typedef virtual axi4_lite_if.mst axi4_lite_vif;
 
 class axi4_lite_driver extends uvm_driver#(axi4_lite_packet);
     `uvm_component_utils(axi4_lite_driver)
+
+    axi4_lite_vif vif;
 
     //  Constructor: new
     function new(string name = "axi4_lite_driver", uvm_component parent);
@@ -21,19 +24,55 @@ class axi4_lite_driver extends uvm_driver#(axi4_lite_packet);
     //  Function: build_phase
     extern function void build_phase(uvm_phase phase);
 
-    //  Function: main_phase
+    //  Task: main_phase
     extern task main_phase(uvm_phase phase);
+
+
+    /*
+    Task: drive_wr_addr_channel
+    This task will be responsible for drive data into the
+    write address channel of the AXI4 Lite
+    */
+    extern task drive_wr_addr_channel();
+
+
     
 endclass: axi4_lite_driver
 
 function void axi4_lite_driver::build_phase(uvm_phase phase);
     super.build_phase(phase);
+    assert(uvm_config_db#(axi4_lite_vif)::get(this, "", "vif", vif));
+        else `uvm_fatal(get_type_name(), "Failed to get virtual interface")
+    
+
+
 endfunction: build_phase
 
 
 task axi4_lite_driver::main_phase(uvm_phase phase);
-    seq_item_port.get_next_item(req);
-    req.print();
-    seq_item_port.item_done();
+    
+    @(negedge vif.arst_n);
+    @(posedge vif.arst_n);
+
+    forever begin
+        fork
+            drive_wr_addr_channel();
+        join
+    end
 endtask: main_phase
 
+task axi4_lite_driver::drive_wr_addr_channel();
+    seq_item_port.get_next_item(req);
+    `uvm_info(get_type_name(), $sformatf("Driver got a transaction: \n%s", req.sprint()), UVM_HIGH)
+    
+    // This channel cannot wait for the ready to raise the valid
+
+    vif.awvalid = 1'b1;
+    vif.awaddr = req.addr;
+
+    @(negedge vif.awready);
+    vif.awvalid = 1'b0;
+    seq_item_port.item_done();
+
+
+endtask : drive_wr_addr_channel

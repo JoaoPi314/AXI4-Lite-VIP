8a1216c32a0b59ab9a9746b428918d185a55fdc2
joao.gomes <joao.melquiades@ee.ufcg.edu.br>
2023-02-28 15:50:52 -0300

    refactor: Change ENUM to match pkg alteration

--- sv/sequences/axi4_lite_wr_data_master_sequence.sv
+++ sv/sequences/axi4_lite_wr_data_master_sequence.sv
@@ -25,5 +25,5 @@ class axi4_lite_wr_data_master_sequence extends axi4_lite_base_sequence;
 endclass: axi4_lite_wr_data_master_sequence
 
 task axi4_lite_wr_data_master_sequence::body();
-    `uvm_do_with(req, {req.active_channel == WR_DATA;})
+    `uvm_do_with(req, {req.active_channel == WR_DATA; req.handshake_type == SEND_FIRST;})
 endtask : body
